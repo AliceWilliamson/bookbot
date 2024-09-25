620eb6cc66108ec14f0a2212645ab6748d4f1cf4
AliceWilliamson <alicewilliamson13@gmail.com>
2024-09-24 22:00:20 -0600

    finished bookbot project

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ def main():
     book = "books/frankenstein.txt"
     text = get_text(book)
     word_count = get_word_count(text)
-    print(f"This document has {word_count} words")
     char_count = count_characters(text)
-    print(f"this is the breakdown of letters in the document: {char_count}")
+    sorted_char_list = get_sorted_list(char_count)
+
+    print(f"--- Begin report of {book} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in sorted_char_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']} character was found {item['num']} times")
+    
+    print("--- End report ---")
+
 
 
 
@@ -13,12 +24,20 @@ def get_word_count(text):
     words = text.split()
     return len(words)
     
-        
+def sort_on(d):
+    return d["num"]
 
 def get_text(path):
     with open(path) as f:
         return f.read()
 
+
+def get_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num" : num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def count_characters(text):
     letter_counts = {}
@@ -29,8 +48,7 @@ def count_characters(text):
             else:
                 letter_counts[char] +=1
             pass
-    sorted_dict = {k: letter_counts[k] for k in sorted(letter_counts)}
-    return sorted_dict
+    return letter_counts
 
 
 main()
